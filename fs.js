0292f84572414aa525a97da877acfbd8bdd19107
Jessica McCabe <mccabejessica88@gmail.com>
2020-01-21 20:21:46 +0000

    Update fs.js
    Step 06

--- fs.js
+++ fs.js
@@ -9,3 +9,16 @@ const fsConfig = {
 const fsCredentials = '&client_id=' + fsConfig.client_id + '&client_secret=' + fsConfig.client_secret + '&v=20190101';
 
 console.log(fsConfig.base_url + 'near=Waterford,IE' + fsCredentials);
+
+axios.get(fsConfig.base_url + 'near=Waterford,IE' + fsCredentials).then(function(response) {
+    var venues = response.data.response.groups[0].items;
+    const venueCollection = [];
+    for (let i = 0; i < venues.length; i++) {
+        const singleVenue = venues[i].venue;
+        const venue = {
+            name: singleVenue.name,
+        };
+        venueCollection.push(venue);
+    }
+    console.log(venueCollection);
+});
